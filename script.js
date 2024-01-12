033cf8596f92defe2163acb11d76f379050d5272
mv <152996178+MVugi@users.noreply.github.com>
2024-01-12 13:17:16 +0100

    Feat: Created page for RPS similar to landing page project added images and simple play function to 3 choice buttons

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+let playerSelection;
+let computerSelection;
+let gameCounter = 0;
+let winCounter = 0;
+let loseCounter = 0;
+
+const playerImage = document.querySelector("#player-image");
+const playerBorder = document.querySelector("#player-image-container");
+const computerImage = document.querySelector("#computer-image");
+const computerBorder = document.querySelector("#computer-image-container");
+
+const resultBox = document.querySelector(".result-box");
+const resultBoxSub = document.createElement("p");
+
+const winBox = document.querySelector(".win-box");
+const loseBox = document.querySelector(".lose-box");
+const counterBox = document.querySelector(".counter-box");
+
+const historyList = document.querySelector("#history");
+
+let resetGame = function () {
+  gameCounter = 0;
+  winCounter = 0;
+  loseCounter = 0;
+  playerImage.src = "./images/you.png";
+  computerImage.src = "./images/computer.png";
+};
+
 function getComputerChoice() {
   let computerChoice;
   let x = Math.random();
@@ -11,54 +39,85 @@ function getComputerChoice() {
   return computerChoice;
 }
 
+const rockButton = document.querySelector("#rock");
+rockButton.addEventListener("click", () => {
+  playerSelection = rockButton.getAttribute("id");
+  playRound(playerSelection, computerSelection);
+});
+
+const paperButton = document.querySelector("#paper");
+paperButton.addEventListener("click", () => {
+  playerSelection = paperButton.getAttribute("id");
+  playRound(playerSelection, computerSelection);
+});
+
+const scissorsButton = document.querySelector("#scissors");
+scissorsButton.addEventListener("click", () => {
+  playerSelection = scissorsButton.getAttribute("id");
+  playRound(playerSelection, computerSelection);
+});
+
+/* Plays Round: 
+gets player's choice from button - computer's from getComputerChoice()
+sets image sources according to picks
+compares choices and sets border colors accordingly
+increments counters for: wins,losses,rounds
+returns results and adds new line to result-list */
+
 function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-  let roundResult;
-  if (!(playerSelection === computerSelection)) {
-    if (
-      playerSelection === "rock" ||
-      playerSelection === "paper" ||
-      playerSelection === "scissors"
+  while (winCounter < 5 && loseCounter < 5) {
+    let roundResult;
+    resultBoxSub.innerHTML = "";
+    computerSelection = getComputerChoice();
+    playerImage.src = `./images/${playerSelection}.png`;
+    computerImage.src = `./images/${computerSelection}.png`;
+
+    if (playerSelection === computerSelection) {
+      roundResult = "Draw!";
+      resultBox.innerHTML = "DRAW!";
+      resultBoxSub.innerHTML = "";
+      playerBorder.style.borderColor = "orange";
+      computerBorder.style.borderColor = "orange";
+    } else if (
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
+      (playerSelection === "scissors" && computerSelection === "paper")
     ) {
-      if (
-        (playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "paper" && computerSelection === "rock") ||
-        (playerSelection === "scissors" && computerSelection === "paper")
-      ) {
-        winCounter++;
-        roundResult =
-          "You won! " + playerSelection + " beats " + computerSelection + "!";
-      } else {
-        roundResult =
-          "You lost! " + computerSelection + " beats " + playerSelection + "!";
-        loseCounter++;
-      }
-      gameCounter++;
+      playerBorder.style.borderColor = "green";
+      computerBorder.style.borderColor = "red";
+      winCounter++;
+      resultBox.textContent = "Round won!";
+      resultBoxSub.textContent = `Your ${playerSelection} beats ${computerSelection}`;
+      roundResult =
+        "You won! " + playerSelection + " beats " + computerSelection + "!";
+    } else {
+      playerBorder.style.borderColor = "red";
+      computerBorder.style.borderColor = "green";
+      loseCounter++;
+      resultBox.textContent = "Round lost!";
+      resultBoxSub.textContent = `The computer's ${computerSelection} beats your ${playerSelection}`;
+      roundResult =
+        "You lost! " + computerSelection + " beats " + playerSelection + "!";
     }
-  } else {
-    roundResult = "Invalid Input or Draw";
+    gameCounter++;
+    resultBox.appendChild(resultBoxSub);
+    winBox.innerHTML = `WON: ${winCounter}`;
+    loseBox.innerHTML = `LOST: ${loseCounter}`;
+    counterBox.innerHTML = `Games played: ${gameCounter}`;
+    const historyItem = document.createElement("li");
+    historyItem.textContent = roundResult;
+
+    //reverse order of list in css to always show newest round result on top
+    historyList.appendChild(historyItem);
+    return roundResult;
   }
-  console.log("Gamecounter: " + gameCounter);
-  console.log("Wincounter: " + winCounter + "\n Losecounter: " + loseCounter);
-  return roundResult;
-}
-
-let playerSelection;
-let computerSelection;
-let gameCounter = 0;
-let winCounter = 0;
-let loseCounter = 0;
-let bestOfFive = true;
-
-while (bestOfFive) {
-  playerSelection = prompt("Enter your choice: ");
-  computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
-    if (winCounter === 3){
-        alert (`You won the best-of-3 game! \nFinal score: ${winCounter}` + " : " + loseCounter);
-        bestOfFive = false;
-    } else if (loseCounter === 3) {
-        alert (`You lost the best-of-3 game! \nFinal score: ${loseCounter}` + " : " + winCounter);
-        bestOfFive = false;
-    }
+  if (winCounter === 5) {
+    window.alert("You Won! Hit confirm to play again...");
+  } else if (loseCounter === 5) {
+    window.alert("You lost! Hit confirm to try again...");
+  }
+  resetGame();
+  winBox.innerHTML = `WON: ${winCounter}`;
+  loseBox.innerHTML = `LOST: ${loseCounter}`;
+  counterBox.innerHTML = `Games played: ${gameCounter}`;
 }
